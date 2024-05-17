dee174046410b7b2f8c4a3796050edf0d14e92cb
Cecilia Gio Alonso Goncalves <ceciggonca@gmail.com>
2024-05-17 15:14:39 -0300

    criação do workspace

---
+++ meu_workspace/src/build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cecilia/Documentos/github/prova1-m6/meu_workspace/src/build/turtlesim/turtlesim__py
+# Build directory: /home/cecilia/Documentos/github/prova1-m6/meu_workspace/src/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
